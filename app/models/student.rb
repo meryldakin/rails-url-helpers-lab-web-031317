370e4871f475c0624232a89540a1e02a09a1548c
meryldakin <meryl.k.dakin@gmail.com>
2017-04-10 11:13:50 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,9 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_message
+    !self.active ? "This student is currently inactive." : "This student is currently active."
+  end
+
 end
---
+++ db/migrate/002_add_student_active.rb
@@ -0,0 +1,6 @@
+class AddStudentActive < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, default: false
+  end
+
+end
